aa87d09a05ecedf5b495137fbefb1d70f5e325fa
Yusi Fayed <Yusi Fayed@MSI>
2018-08-15 09:57:32 +0700

    initial commit

---
+++ src/j11/latihan01.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package j11;
+
+import java.util.Locale;
+
+/**
+ *
+ * @author Yusi Fayed
+ */
+public class latihan01 {
+
+    public static void main(String[] args) {
+      String identitas = "Fairuzy Al Fayed / X PRL 2 / 12";
+      System.out.println("Identitas : "+ identitas);
+    
