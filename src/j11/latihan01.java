7a5e424489934587b09eb21745431b02bc268e12
Yusi Fayed <Yusi Fayed@MSI>
2018-08-15 09:59:07 +0700

    Coba method length & isEmpty.

--- src/j11/latihan01.java
+++ src/j11/latihan01.java
@@ -16,4 +16,13 @@ public class latihan01 {
     public static void main(String[] args) {
       String identitas = "Fairuzy Al Fayed / X PRL 2 / 12";
       System.out.println("Identitas : "+ identitas);
+      String x = "operasi";
+      System.out.println("isi variable x : "+ x);
+      System.out.println("\""+ x + "\" panjang karakter = "+ x.length());
+        
+      System.out.println("x adalah kosong : " + x.isEmpty());
+        
+      String y = "";
+      System.out.println("isi variable y : "+ y);
+      System.out.println("y adalah kosong : " + y.isEmpty());
     
